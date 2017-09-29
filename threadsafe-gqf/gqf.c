c34312de3da41e5e3a519fde3778a29af3cd6154
Prashant Pandey <prashant.prashn@gmail.com>
2017-09-29 18:10:49 -0400

    Added code for the new shuffle-merge.

--- threadsafe-gqf/gqf.c
+++ threadsafe-gqf/gqf.c
@@ -2084,7 +2084,7 @@ void qf_multi_merge(QF *qf_arr[], int nqf, QF *qfr, bool lock, bool spin)
 	int i;
 	QFi qfi_arr[nqf];
 	int flag = 0;
-	int smallest_i = 0;
+	int smallest_idx = 0;
 	uint64_t smallest_key = UINT64_MAX;
 	for (i=0; i<nqf; i++) {
 		qf_iterator(qf_arr[i], &qfi_arr[i], 0);
@@ -2101,20 +2101,20 @@ void qf_multi_merge(QF *qf_arr[], int nqf, QF *qfr, bool lock, bool spin)
 			smallest_key = UINT64_MAX;
 			for (i=0; i<nqf; i++) {
 				if (keys[i] < smallest_key) {
-					smallest_key = keys[i]; smallest_i = i;
+					smallest_key = keys[i]; smallest_idx = i;
 				}
 			}
-			qf_insert(qfr, keys[smallest_i], values[smallest_i], counts[smallest_i],
+			qf_insert(qfr, keys[smallest_idx], values[smallest_idx], counts[smallest_idx],
 								lock, spin);
-			qfi_next(&qfi_arr[smallest_i]);
-			qfi_get(&qfi_arr[smallest_i], &keys[smallest_i], &values[smallest_i],
-							&counts[smallest_i]);
-		} while(!qfi_end(&qfi_arr[smallest_i]));
+			qfi_next(&qfi_arr[smallest_idx]);
+			qfi_get(&qfi_arr[smallest_idx], &keys[smallest_idx], &values[smallest_idx],
+							&counts[smallest_idx]);
+		} while(!qfi_end(&qfi_arr[smallest_idx]));
 
 		/* remove the qf that is exhausted from the array */
-		if (smallest_i < nqf-1)
-			memmove(&qfi_arr[smallest_i], &qfi_arr[smallest_i+1],
-							(nqf-smallest_i-1)*sizeof(qfi_arr[0]));
+		if (smallest_idx < nqf-1)
+			memmove(&qfi_arr[smallest_idx], &qfi_arr[smallest_idx+1],
+							(nqf-smallest_idx-1)*sizeof(qfi_arr[0]));
 		nqf--;
 		if (nqf == 1)
 			flag = 1;
@@ -2186,7 +2186,7 @@ uint64_t qf_shuffle_merge_step(QF *qf_arr[], uint8_t thlds[], uint8_t nqf,
  * This shuffle merge depends on a stateless iterator, since we will be
  * performing the shuffle-merge in-place.
  */
-void qf_shuffle_merge(QF *qf_arr[], uint8_t thlds[], uint8_t nqf, uint64_t
+void qf_shuffle_merge_inplace(QF *qf_arr[], uint8_t thlds[], uint8_t nqf, uint64_t
 											hash_begin, uint64_t hash_end, bool lock, bool spin)
 {
 	uint64_t cur = hash_begin-1; 		//Need to handle the case when hash_begin is 0.
@@ -2195,6 +2195,97 @@ void qf_shuffle_merge(QF *qf_arr[], uint8_t thlds[], uint8_t nqf, uint64_t
 		cur = qf_shuffle_merge_step(qf_arr, thlds, nqf, cur, lock, spin);
 		if (cur == UINT64_MAX)
 			break;
+	}
+}
+
+/**
+ * Perform a shuffle-merge among @nqf cqfs from @qf_arr and put elements in
+ * new cqfs in @qf_arr_new.
+ *
+ * After the shuffle-merge the cqfs in @qf_arr will be destroyed and memory
+ * will be freed.
+ *
+ * This function assumes that the cqfs in @qf_arr_new have already been
+ * initialized by the caller.
+ */
+void qf_shuffle_merge(QF *qf_arr[], uint8_t thlds[], uint8_t nqf, QF
+											*qf_arr_new[], bool lock, bool spin) {
+	QFi qfi_arr[nqf];
+	int flag = 0;
+
+	/* Initialize iterators. */
+	for (int i=0; i<nqf; i++) {
+		qf_iterator(qf_arr[i], &qfi_arr[i], 0);
+	}
+
+	while (!flag) {
+		uint64_t keys[nqf];
+		uint64_t values[nqf];
+		uint64_t counts[nqf];
+		int smallest_idx = 0;
+		uint64_t smallest_key = UINT64_MAX;
+
+		do {
+			/* Get the current key from each level and find the smallest one among
+			 * them and aggregate its count.
+			 * */
+			smallest_key = UINT64_MAX;
+			for (int i=0; i<nqf; i++) {
+				qfi_get(&qfi_arr[i], &keys[i], &values[i], &counts[i]);
+				if (keys[i] < smallest_key) {
+					smallest_key = keys[i];
+					smallest_idx = i;
+				} else if (keys[i] == smallest_key) {
+					counts[smallest_idx] += counts[i];
+				}
+			}
+			/* Smear aggregated count of smallest_key starting from the lowest
+			 * level. */
+			for (uint32_t i = nqf; i <= nqf; i++) {
+				if (counts[smallest_idx] >= thlds[i]) {
+					qf_insert(qf_arr_new[i], keys[smallest_idx], values[smallest_idx],
+										thlds[i], lock, spin);
+					counts[smallest_idx] -= thlds[i];
+				} else if (counts[smallest_idx] > 0) {
+					qf_insert(qf_arr_new[i], keys[smallest_idx], values[smallest_idx],
+										counts[smallest_idx], lock, spin);
+				} else
+					break;
+			}
+			/* Increment the iterator on all the levels that had the smallest_key. */
+			for (int i=0; i<nqf; i++) {
+				if (keys[i] == smallest_key) {
+					qfi_next(&qfi_arr[smallest_idx]);
+					qfi_get(&qfi_arr[smallest_idx], &keys[smallest_idx],
+									&values[smallest_idx], &counts[smallest_idx]);
+				}
+			}
+		} while(!qfi_end(&qfi_arr[smallest_idx]));
+		/* remove the cqf that is exhausted from the array. */
+		if (smallest_idx < nqf-1)
+			memmove(&qfi_arr[smallest_idx], &qfi_arr[smallest_idx+1],
+							(nqf-smallest_idx-1)*sizeof(qfi_arr[0]));
+		nqf--;
+		if (nqf == 1)
+			flag = 1;
+	}
+
+	if (!qfi_end(&qfi_arr[0])) {
+		do {
+			uint64_t key, value, count;
+			qfi_get(&qfi_arr[0], &key, &value, &count);
+			/* Smear aggregated count of smallest_key starting from the lowest
+			 * level. */
+			for (uint32_t i = nqf; i <= nqf; i++) {
+				if (count >= thlds[i]) {
+					qf_insert(qf_arr_new[i], key, value, thlds[i], lock, spin);
+					count -= thlds[i];
+				} else if (count > 0) {
+					qf_insert(qf_arr_new[i], key, value, count, lock, spin);
+				} else
+					break;
+			}
+		} while(!qfi_next(&qfi_arr[0]));
 	}
 }
 
