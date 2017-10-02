794a56943ebb410233e5d3613b14658a72bfec8c
Prashant Pandey <prashant.prashn@gmail.com>
2017-10-02 10:57:54 -0400

    Adding cascade filter code.

--- cascadefilter.h
+++ cascadefilter.h
@@ -16,7 +16,6 @@
  * =====================================================================================
  */
 
-
 #ifdef _CASCADEFILTER_H_
 #define _CASCADEFILTER_H_
 
@@ -31,10 +30,54 @@
 #define NUM_MAX_FILTERS 10;
 
 class CascadeFilter {
+
+	CascadeFilter();
+
+	/* Increment the counter for this key/value pair by count. */
+	bool insert(QF *qf, uint64_t key, uint64_t value, uint64_t count, bool lock,
+							bool spin);
+
+	/* Remove count instances of this key/value combination. */
+	void remove(QF *qf, uint64_t key, uint64_t value, uint64_t count, bool lock);
+
+	/* Replace the association (key, oldvalue, count) with the association
+		 (key, newvalue, count). If there is already an association (key,
+		 newvalue, count'), then the two associations will be merged and
+		 their counters will be summed, resulting in association (key,
+		 newvalue, count' + count). */
+	void replace(QF *qf, uint64_t key, uint64_t oldvalue, uint64_t newvalue);
+
+	/* Return the number of times key has been inserted, with the given
+		 value, into qf. */
+	uint64_t count_key_value(const QF *qf, uint64_t key, uint64_t value) const;
+
+	private:
 	QF *filters[NUM_MAX_FILTERS];
 	uint32_t thresholds[NUM_MAX_FILTERS];
 	uint64_t sizes[NUM_MAX_FILTERS];
 };
 
+class CascadeFilterIterator {
+
+	CascadeFilterIterator(const CascadeFilter *cf, uint64_t position);
+
+	/* Returns 0 if the iterator is still valid (i.e. has not reached the
+		 end of the QF. */
+	int get(uint64_t *key, uint64_t *value, uint64_t *count, uint32_t *level);
+
+	/* Advance to next entry.  Returns whether or not another entry is
+		 found.  */
+	int next();
+
+	/* Check to see if the if the end of the QF */
+	int end();
+
+	private:
+	const CascadeFilter cf;
+	QFi qfi_arr[nqf];
+	uint32_t cur_level;
+	uint64_t smallest_idx;
+	uint64_t smallest_key;
+};
 
 #endif
